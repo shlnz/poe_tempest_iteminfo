d3dc938bfaef10c9b4218decb5de0235a4426c32
shlnz <jens.thom@ruhr-uni-bochum.de>
2015-07-26 17:26:50 +0200

    Added vote function

--- poe_tempest.py
+++ poe_tempest.py
@@ -5,10 +5,16 @@ import winsound
 import configparser
 import psutil
 import subprocess
+import winreg
+import platform
+import errno
+import re
+import requests
 
 from urllib.request import urlopen
 from datetime import datetime, timedelta
 from time import sleep
+from itertools import islice
 
 
 GREAT = {}
@@ -20,9 +26,14 @@ def play_sound():
     winsound.PlaySound(fname, winsound.SND_FILENAME)
 
 
-def full_hour():
+def next_full_hour():
     ''' returns the next full hour '''
     return datetime.now().replace(minute=0, second=0, microsecond=0) + timedelta(hours=1)
+
+
+def last_full_hour():
+    ''' returns the next full hour '''
+    return datetime.now().replace(minute=0, second=0, microsecond=0)
 
 
 def current_time_str():
@@ -125,7 +136,7 @@ def handle_great_tempests(temps, play_mp3):
     global GREAT
 
     if datetime.now() > NEXT_FULL_HOUR:
-        NEXT_FULL_HOUR = full_hour()
+        NEXT_FULL_HOUR = next_full_hour()
         GREAT = {}
 
     for map_name, prefs in temps.items():
@@ -153,39 +164,211 @@ def read_config():
     config.read('config.ini')
 
     try:
-        auto_hot_key = config['Path']['AutoHotKey']
-        file_to_start = config['Path']['ItemInfo']
+        # auto_hot_key = config['Paths']['AutoHotKey']
+        file_to_start = config['Paths']['ItemInfo']
         reload_time = int(config['RefreshInterval']['reload'])
+        # poe_path = config['Paths']['poePath']
         if reload_time < 1:
             reload_time = 1
         play_mp3 = config['PlaySounds']['play']
     except KeyError:
-        config['Path'] = {'AutoHotKey': 'C:\\path\\to\\AutoHotKey.exe',
-                          'ItemInfo': 'C:\\path\\to\\ItemInfo.ahk'}
+        config['Paths'] = {'AutoHotKey': 'C:\\path\\to\\AutoHotKey.exe',
+                           'ItemInfo': 'C:\\path\\to\\ItemInfo.ahk'}
         config['RefreshInterval'] = {'reaload': '5'}
-        config['PlaySounds'] = {'play': 'True'}
+        config['PlaySounds'] = {'play': 'on'}
         with open('config.ini', 'w') as configfile:
             config.write(configfile)
         print("Wrote new config.ini.\nYou need to adjust this config file first.")
         exit_with_input()
 
-    return auto_hot_key, reload_time, file_to_start, play_mp3
+    return reload_time, file_to_start, play_mp3
+
+
+def read_config_part(topic, item):
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+
+    try:
+        value = config[topic][item]
+    except KeyError:
+        print("ERROR: Could not read [%s] %s from config.ini." % (topic, item))
+        exit_with_input()
+
+    return value
+
+
+def verify_log_data_and_vote(map_name, base_name, suffix_name=None):
+    response = urlopen('http://poetempest.com/api/v0/maps').read()
+    maps = json.loads(response.decode('utf-8'))
+
+    response = urlopen('http://poetempest.com/api/v0/tempests').read()
+    tempests = json.loads(response.decode('utf-8'))
+
+    # verify map correctness
+    if map_name not in maps:
+        return
+
+    # verify bases correctness
+    if base_name not in tempests['bases']:
+        return
+
+    # verify suffix correctness
+    if suffix_name is not None:
+        if suffix_name not in tempests['suffixes']:
+            return
+    else:
+        suffix_name = 'none'
+
+    # vote for tempest on map
+    headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+    data = {'map': map_name, 'base': base_name, 'suffix': suffix_name}
+    url = 'http://poetempest.com/api/v0/vote'
+    response = requests.post(url, data=data, headers=headers)
+    if response.status_code == requests.codes.ok:
+        print('[%s] Voting successful for Map: %s, base: %s, suffix: %s' % (current_time_str, map_name, base_name, suffix_name))
+
+    return
+
+
+def delete_poe_log(path):
+    try:
+        os.remove(path)
+        print('Logfile gone')
+    except OSError:
+        pass
+
+
+def read_poe_log(path, last_pos=0):
+    last_h = last_full_hour()
+    next_h = next_full_hour()
+    try:
+        with open(path, 'r') as f:
+            for i, line in enumerate(islice(f, last_pos, None)):
+                # check if the chat message is still in the current hour
+                date = line[:19].replace('/', ' ').replace(':', ' ').split(' ')
+                date = datetime(*[int(i) for i in date])
+
+                if last_h < date < next_h:
+                    line = line[19:]
+                    match = re.findall(r'[\d\/\:\ ]{10,}287 \[[\w\s]*\]\ [\&\#]*[\w\_\-]*\:\ TEMPEST[\:\ ]{2,}(.*?)[\,\;\ ]{2,}(.*?)$', line, re.DOTALL)
+                    if match and len(match[0]) == 2:
+                        # TODO: need to replace it with a regex
+                        map_name, prefs = match[0]
+                        map_name = map_name.replace(' ', '_').lower()
+                        if 'of' in prefs.lower():
+                            temps = prefs.split('of')
+                            base = temps[0].lower().replace('tempest', '').strip()
+                            suffix = temps[1].lower().strip()
+                            verify_log_data_and_vote(map_name, base, suffix)
+                        elif 'none' in prefs.lower():
+                            verify_log_data_and_vote(map_name, 'none')
+                        else:
+                            temps = prefs.lower().lstrip().split(' ')
+                            base = temps[0].strip()
+                            verify_log_data_and_vote(map_name, base)
+
+            last_pos += i
+
+        return last_pos
+    except IOError:
+        return last_pos
+
+
+def find_paths_in_registry():
+    ''' Try to find registry entrys for AutoHotkey and Path of Exile '''
+    programs = {'AutoHotkey': 'DisplayIcon', 'Steam App 238960': 'InstallLocation', '90A4562F-D4A1-4B65-906D-41F236CF6902': 'InstallSource'}
+    paths = {}
+    ignore = read_config_part('Registry', 'ignore')
+
+    if ignore != 'yes':
+        architec = platform.machine()
+        if architec == 'AMD64':
+            architec_keys = winreg.KEY_WOW64_64KEY | winreg.KEY_WOW64_32KEY
+        else:
+            architec_keys = 0
+
+        try:
+            regconn = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+            key = winreg.OpenKey(regconn, 'SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall', winreg.KEY_READ | architec_keys)
+
+            for i in range(0, winreg.QueryInfoKey(key)[0]):
+                subkey = winreg.EnumKey(key, i)
+
+                if subkey in programs.keys():
+                    skey = winreg.OpenKey(key, subkey)
+
+                    try:
+                        install_location = winreg.QueryValueEx(skey, programs[subkey])[0]
+                        if subkey == 'Steam App 238960' or subkey == '90A4562F-D4A1-4B65-906D-41F236CF6902':
+                            paths['POE'] = install_location
+                        else:
+                            paths[subkey] = install_location
+                    except OSError as e:
+
+                        if e.errno == errno.ENOENT:
+                            pass
+        except (WindowsError, EnvironmentError):
+            print('Unable to Connect to the Window Registry and read keys')
+        finally:
+            key.Close()
+
+    # TODO: check, if POE is installed via steam and normal then use config instead
+    if 'AutoHotkey' not in paths.keys():
+        autohotkey_path = read_config_part('Paths', 'AutoHotKey')
+
+        if os.path.isfile(autohotkey_path):
+            print("Successfully loaded AutoHotKey path from config file")
+            paths['AutoHotkey'] = autohotkey_path
+        print('AutoHotkey not found in registry')
+
+    if 'POE' not in paths.keys():
+        poe_path = read_config_part('Paths', 'poePath')
+
+        if os.path.isfile(os.path.join(poe_path, 'logs', 'Client.txt')):
+            print("Successfully loaded poePath from config file")
+            paths['POE'] = poe_path
+
+        else:
+            print('Could not find Path of Exile in registry. You need to set up the right path manually in the config.ini')
+            exit_with_input()
+
+    for key in paths.keys():
+
+        if key == 'AutoHotkey':
+            # TODO: just testing
+            paths[key] = paths[key][:-4] + 'A32.exe'
+
+        elif key == 'POE':
+            paths['POE'] += '\logs\Client.txt'
+
+    return paths
 
 
 if __name__ == '__main__':
-    # TODO: READ LOGFILE FROM POE
-    NEXT_FULL_HOUR = full_hour()
-    prog_path, reload_time, file_to_start, play_mp3 = read_config()
+    last_pos = 0
+    paths = find_paths_in_registry()
+
+    delete = read_config_part('POE-Log', 'delete')
+    if delete.lower() == 'yes':
+        delete_poe_log(paths['POE'])
+
+    NEXT_FULL_HOUR = next_full_hour()
+    reload_time, file_to_start, play_mp3 = read_config()
     try:
         while True:
             clear()
-            end_autohotkey(prog_path)
+            end_autohotkey(paths['AutoHotkey'])
+
             print('[%s] Grabbing new data...' % current_time_str())
+
             temps_dict = get_current_tempests()
             handle_great_tempests(temps_dict, play_mp3)
             write_to_map_list(temps_dict, file_to_start)
+
+            last_pos = read_poe_log(paths['POE'], last_pos)
+
             print('[%s] Done. Next try in %d min. Restarting "AutoHotKey"' % (current_time_str(), reload_time))
-            subprocess.Popen([prog_path, file_to_start])
+            subprocess.Popen([paths['AutoHotkey'], file_to_start])
             sleep(reload_time * 60)
     except KeyboardInterrupt:
-        end_autohotkey(prog_path)
+        end_autohotkey(paths['AutoHotkey'])
